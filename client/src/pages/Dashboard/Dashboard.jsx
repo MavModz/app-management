f82987c2cefe1bbae81f74bf5e6f07e66cbf1aa2
mavmodz <maverickjetman07@gmail.com>
2023-11-21 18:31:47 +0530

    dashboard updates

--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../../Components/Header/Header';
 import Study from '../../assets/images/study.png';
+import Thumb from '../../assets/images/user-2.jpg';
 import './dashboard.css';
 
 function Dashboard() {
@@ -9,23 +10,42 @@ function Dashboard() {
       <Header />
       <div className="dashboard-conatiner">
         <div className="dashboard-wrapper">
-          <div className="banner-container h-80 rounded-xl flex justify-around items-center">
+          <div className="banner-container h-80 rounded-2xl flex justify-around items-center">
             <div className="top-banner">
-              <h1 className='banner-text text-2xl'>Welcome back to your Dashboard</h1>
-              <p>You’ve learned 70% of your goal this week! <br />
+              <h1 className='banner-text text-5xl mb-4'>Welcome to your Dashboard</h1>
+              <p className='text-xl'>You’ve learned <span className='banner-span'><strong>70% of your goal</strong></span> this week! <br />
                 Keep it up and improve your results!
               </p>
             </div>
             <div className='banner-svg'>
-              <img src= {Study} alt="student studying on the table" className='h-72' />
+              <img src={Study} alt="student studying on the table" className='h-72' />
             </div>
           </div>
           <div className="stats-wrapper flex gap-8 justify-around h-96 mt-5">
-            <div className="course-progress w-5/12">
-              <p>progress data</p>
+            <div className="course-progress w-5/12 px-8">
+              <div className="course-heading my-4">
+                <h1 className='progress-heading text-2xl'>progress data</h1>
+              </div>
+              <div className="course-details">
+                <div className='course-info flex gap-4'>
+                  <div className="course-thumbnail flex items-center">
+                    <img src= {Thumb} alt="course thumbnail" className='course-image' />
+                  </div>
+                  <div className="course-data">
+                    <p>Coding</p>
+                    <h2>Learn JavaSript</h2>
+                    <h3>Learn how to use JavaScript — a powerful and flexible programming language for adding website interactivity.</h3>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div className="schedule w-5/12">
-              <p>schedule data</p>
+            <div className="schedule w-5/12 px-8">
+              <div className="schedule-heading mt-4">
+                <h1 className='schedule-heading text-2xl'>schedule data</h1>
+              </div>
+              <div className="schedule-graph">
+                <p>graph</p>
+              </div>
             </div>
           </div>
         </div>
